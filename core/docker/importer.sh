5bfdc7645488764dc03f978b6e9f5cbbb3277b7c
MattyJ007 <matthew.dickie@jembi.org>
2020-02-03 11:16:13 +0200

    Add sleep logic to config import
    The OpenHIM takes a few seconds to become responsive after its docker
    container starts up. We therefore require some logic to check the
    heartbeat endpoint of the HIM to ensure that the API is responsive
    before attempting to post the config import.
    
    OHIE-310

--- core/docker/importer.sh
+++ core/docker/importer.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-curl -X POST --insecure -u "$OPENHIM_API_USERNAME:$OPENHIM_API_PASSWORD" -H "Content-Type: application/json" -d @openhim-import.json "$OPENHIM_API_SERVER/metadata" -v
+OPENHIM_RESPONSE="";
+
+while [ "$OPENHIM_RESPONSE" != "200" ]
+do
+  echo "OpenHIM not ready ( $OPENHIM_RESPONSE ) - sleeping"
+  sleep 2
+  OPENHIM_RESPONSE=$(curl -X GET --insecure --write-out %{http_code} --silent --output /dev/null "$OPENHIM_API_SERVER/heartbeat");
+done
+
+echo -e "\nSTART Posting OpenHIM Config\n----------------------------\n"
+curl --insecure -u "$OPENHIM_API_USERNAME:$OPENHIM_API_PASSWORD" -H "Content-Type: application/json" -d @openhim-import.json "$OPENHIM_API_SERVER/metadata" -v
+echo -e "\n\n--------------------------\nEND Posting OpenHIM Config\n"
