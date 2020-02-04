8a912abc4942d2c5a77c91c2d390acfa8c0de4a6
MattyJ007 <matthew.dickie@jembi.org>
2020-02-04 14:28:57 +0200

    Switch from bash scripts to node
    To make these scripts easier to maintain.
    
    This logic does not work yet - just commiting it to get some feedback.
    
    OHIE-310

---
+++ core/docker/importer/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const {importOpenHIMConfig} = require('./openhim')
+
+importOpenHIMConfig()
---
+++ core/docker/importer/openhim.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+const waitOn = require('wait-on')
+
+const OPENHIM_API_HOSTNAME =
+  process.env.OPENHIM_API_SERVER || 'https://openhim-core'
+const OPENHIM_API_PASSWORD =
+  process.env.OPENHIM_API_PASSWORD || 'openhim-password'
+const OPENHIM_API_PORT = process.env.OPENHIM_API_PORT || 8080
+const OPENHIM_API_USERNAME =
+  process.env.OPENHIM_API_USERNAME || 'root@openhim.org'
+
+exports.importOpenHIMConfig = () => {
+  const opts = {
+    resources: [`${OPENHIM_API_HOSTNAME}:${OPENHIM_API_PORT}/heartbeat`],
+    delay: 1000, // initial delay in ms, default 0
+    interval: 2000, // poll interval in ms, default 250ms
+    timeout: 30000, // timeout in ms, default Infinity
+    tcpTimeout: 1000, // tcp timeout in ms, default 300ms
+    window: 1000, // stabilization time in ms, default 750ms
+    auth: {
+      user: OPENHIM_API_USERNAME,
+      pass: OPENHIM_API_PASSWORD
+    },
+    strictSSL: false
+  }
+
+  // Usage with promises
+  waitOn(opts)
+    .then(function() {
+      // once here, all resources are available
+      openHIMConfig()
+    })
+    .catch(function(err) {
+      console.error('Failed to import OpenHIM config: ', err)
+    })
+}
+
+const openHIMConfig = () => {
+  const authHeader = new Buffer.from(
+    `${OPENHIM_API_USERNAME}:${OPENHIM_API_PASSWORD}`
+  ).toString('base64')
+
+  const jsonData = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'openhim-import.json'))
+  )
+
+  const data = JSON.stringify(jsonData)
+
+  const options = {
+    hostname: OPENHIM_API_HOSTNAME,
+    port: OPENHIM_API_PORT,
+    path: '/metadata',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      Authorization: `Basic ${authHeader}`
+    }
+  }
+
+  const req = https.request(options, res => {
+    console.log(`Status code: ${res.statusCode}`)
+  })
+
+  req.on('error', error => {
+    console.error('Failed to import OpenHIM config: ', error)
+  })
+
+  req.write(data)
+  req.end()
+}
