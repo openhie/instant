4384e4e81db35f76869b5267f8a7e8903423196b
Martin Brocker <martin.brocker@jembi.org>
2020-02-20 15:49:36 +0200

    Add ingress file for core and console
    To expose the services via a domain name for better referencing

--- core/kubernetes/k8s.sh
+++ core/kubernetes/k8s.sh
@@ -3,6 +3,11 @@
 if [ "$1" == "up" ]; then
     kubectl apply -f ./openhim
     kubectl get services
+    kubectl get ingress
+
+    # create HOST entry for ingress
+    sudo sed -i "/HOST alias for kubernetes Minikube/d" /etc/hosts
+    echo -e "\n$(minikube ip) $(kubectl get ingress -o jsonpath="{..host}") # HOST alias for kubernetes Minikube" | sudo tee -a /etc/hosts
 elif [ "$1" == "down" ]; then
     kubectl delete deployment openhim-console-deployment
     kubectl delete deployment openhim-core-deployment
@@ -13,6 +18,7 @@ elif [ "$1" == "destroy" ]; then
     kubectl delete persistentvolumeclaims --all --grace-period 2
     kubectl delete cronjobs --all --grace-period 2
     kubectl delete pods --all --grace-period 2
+    kubectl delete ingress --all --grace-period 2
 else
     echo "Valid options are: up, down, or destroy"
 fi
