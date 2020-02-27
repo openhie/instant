ec0aea5f4f278df532ba26593659acb2242d19f9
Ryan Crichton <ryan@jembi.org>
2020-02-27 16:45:03 +0200

    Fix mapping mediator dhis2 sample config

--- healthworkforce/test.sh
+++ healthworkforce/test.sh
@@ -1,2 +1,2 @@
 echo -e "\nMapping medaitor test..."
-curl https://openhim-core.ssl.instant/dhis2part1 -X POST -k -H "Content-Type: application/fhir+json" -f && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
+curl https://openhim-core.ssl.instant/dhis2part1 -X POST -k -H "Content-Type: application/json" -f && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
