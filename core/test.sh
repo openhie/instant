b4db0ec9e0920b9b1af57efc8f20576bb0d239be
Ryan Crichton <ryan@jembi.org>
2020-02-27 14:38:17 +0200

    Correct mapper labels and add test names

--- core/test.sh
+++ core/test.sh
@@ -1,1 +1,2 @@
+echo -e "\nHAPI FHIR test..."
 curl https://openhim-core.ssl.instant/hapi-fhir-jpaserver/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ resourceType: "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
--- healthworkforce/test.sh
+++ healthworkforce/test.sh
@@ -1,1 +1,2 @@
+echo -e "\nMapping medaitor test..."
 curl https://openhim-core.ssl.instant/dhis2part1 -X POST -k -H "Content-Type: application/fhir+json" -f && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
