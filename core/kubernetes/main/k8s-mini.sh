ffc6996d8f971f5161866cf923da32c2d86c110d
MattyJ007 <matthew.dickie@jembi.org>
2020-02-27 11:32:19 +0200

    Change k8s script name
    This script is specific to minikibe deploys. A different script will be
    needed for AWS
    
    OHM-972

--- core/kubernetes/main/k8s-mini.sh
+++ core/kubernetes/main/k8s-mini.sh
@@ -6,6 +6,9 @@ if [ "$1" == "up" ]; then
     minikube addons enable ingress
     kubectl apply -k $kustomizationFilePath
 
+    kubectl get services
+    kubectl get ingress
+
     # create HOST entry for ingress
     sudo sed -i "/HOST alias for kubernetes Minikube/d" /etc/hosts
     echo -e "\n$(minikube ip) $(kubectl get ingress -o jsonpath="{..host}") # HOST alias for kubernetes Minikube" | sudo tee -a /etc/hosts
@@ -16,9 +19,6 @@ elif [ "$1" == "down" ]; then
     kubectl delete deployment hapi-fhir-server-deployment
     kubectl delete deployment hapi-fhir-mysql-deployment
 elif [ "$1" == "destroy" ]; then
-    # delete host entry on destroy
-    sudo sed -i "/HOST alias for kubernetes Minikube/d" /etc/hosts
-
     kubectl delete -k $kustomizationFilePath
 else
     echo "Valid options are: up, down, or destroy"
