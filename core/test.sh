256226caa07f65d0e2de77a6f65408ea65f2d5ba
Ryan Crichton <ryan@jembi.org>
2020-02-28 14:09:00 +0200

    Update script to support deploying via docker, update docker scripts

--- core/test.sh
+++ core/test.sh
@@ -1,2 +1,5 @@
 echo -e "\nHAPI FHIR test..."
-curl https://openhim-core.ssl.instant/hapi-fhir-jpaserver/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ resourceType: "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
+
+HOST=$1
+
+curl https://$HOST/hapi-fhir-jpaserver/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ resourceType: "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
--- healthworkforce/test.sh
+++ healthworkforce/test.sh
@@ -1,2 +1,5 @@
 echo -e "\nMapping medaitor test..."
-curl https://openhim-core.ssl.instant/dhis2part1 -X POST -k -H "Content-Type: application/json" -f && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
+
+HOST=$1
+
+curl https://$HOST/dhis2part1 -X POST -k -H "Content-Type: application/json" -f && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
