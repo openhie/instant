1f917ce14d3ad6c369e873d415dbe3fb5219cb0a
MattyJ007 <matthew.dickie@jembi.org>
2020-03-02 15:49:20 +0200

    Setup Instant HIE Core package on AWS
    The AWS scripts required a different setup mechanism compared to the
    minikube setup. Minikube used host name aliasing on the local machine to
    allow the openhim console to connect to the openhim core without having
    to know the exact url of the core.
    
    That mechanism couldn't work for AWS as the hostnames in this case
    weren't IPs but URLs provided by AWS which could not be used in the
    local HOST file.
    
    This mechanism sets up the Hapi FHIR and OpenHIM core in the first part
    of the script and gets their urls. The OpenHIM Core url is then
    substituted directly into the openhim console config file which is then
    used to create a configmap. Therefore the configmap contains a reference
    to the OpenHIM Core's api endpoint - which shouldn't change.
    
    OHM-972

--- core/kubernetes/main/k8s-aws.sh
+++ core/kubernetes/main/k8s-aws.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 kustomizationFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+openhimConsoleVolumePath="${kustomizationFilePath}/openhim/volume/openhim-console/default.json"
+openhimApiUrl=''
+fhirServerUrl=''
+openhimConsoleUrl=''
 
 if [ "$1" == "up" ]; then
     kubectl apply -k $kustomizationFilePath
 
-    kubectl get services
-    kubectl get ingress
+    openhimApiUrl=$(kubectl get service openhim-core-service -o=jsonpath={.status.loadBalancer.ingress[0].hostname})
+    fhirServerUrl=$(kubectl get service hapi-fhir-server-service -o=jsonpath={.status.loadBalancer.ingress[0].hostname})
+
+    printf "\n\nOpenHIM Api Url\n---------------\n"$openhimApiUrl"\n"
+    printf "\nHAPI FHIR Url\n--------------\n"$fhirServerUrl"\n\n\n"
+
+    sed -i -E "s/(\"host\": \")\S*(\")/\1${openhimApiUrl}\2/" $openhimConsoleVolumePath
+    sed -i -E "s/(\"port\": )\S*(,)/\18082\2/" $openhimConsoleVolumePath
+
+    kubectl apply -k $kustomizationFilePath/openhim
+
+    openhimConsoleUrl=$(kubectl get service openhim-console-service -o=jsonpath={.status.loadBalancer.ingress[0].hostname})
+
+    printf "\n\nOpenHIM Console Url\n-------------------\nhttp://"$openhimConsoleUrl"\n\n"
 
 elif [ "$1" == "down" ]; then
     kubectl delete deployment openhim-console-deployment
