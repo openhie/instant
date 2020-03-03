20598151c4b0800367630e968a0225f132cb1860
MattyJ007 <matthew.dickie@jembi.org>
2020-03-03 12:02:16 +0200

    Remove ingress logic from minikube script
    This is to merge the minikube and aws script into one more generic flow.
    Ingress worked well to make the urls pretty but that isn't needed here
    
    OHM-972

--- core/kubernetes/main/k8s-mini.sh
+++ core/kubernetes/main/k8s-mini.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 kustomizationFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+openhimConsoleVolumePath="${kustomizationFilePath}/openhim/volume/openhim-console/default.json"
+minikubeIP=''
 
 if [ "$1" == "up" ]; then
-    minikube addons enable ingress
     kubectl apply -k $kustomizationFilePath
 
-    # create HOST entry for ingress
-    sudo sed -i "/HOST alias for kubernetes Minikube/d" /etc/hosts
-    echo -e "\n$(minikube ip) $(kubectl get ingress -o jsonpath="{..host}") # HOST alias for kubernetes Minikube" | sudo tee -a /etc/hosts
+    minikubeIP=$(minikube ip)
+    corePort=$(kubectl get service openhim-core-service -o=jsonpath={.spec.ports[0].nodePort})
+    consolePort=$(kubectl get service openhim-console-service -o=jsonpath={.spec.ports[0].nodePort})
+
+    # Injecting minikube ip as the hostname of the OpenHIM Core into Console config file
+    sed -i -E "s/(\"host\": \")\S*(\")/\1${minikubeIP}\2/" $openhimConsoleVolumePath
+
+    # Injecting OpenHIM Core port into Console config file
+    sed -i -E "s/(\"port\": )\S*(,)/\1${corePort}\2/" $openhimConsoleVolumePath
+
+    printf "\n\nOpenHIM Console Url\n-------------------\nhttp://"$minikubeIP":"$consolePort"\n\n"
+
 elif [ "$1" == "down" ]; then
     kubectl delete deployment openhim-console-deployment
     kubectl delete deployment openhim-core-deployment
@@ -16,9 +26,6 @@ elif [ "$1" == "down" ]; then
     kubectl delete deployment hapi-fhir-server-deployment
     kubectl delete deployment hapi-fhir-mysql-deployment
 elif [ "$1" == "destroy" ]; then
-    # delete host entry on destroy
-    sudo sed -i "/HOST alias for kubernetes Minikube/d" /etc/hosts
-
     kubectl delete -k $kustomizationFilePath
 else
     echo "Valid options are: up, down, or destroy"
