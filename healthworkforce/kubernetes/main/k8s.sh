276cd8f5d50e87e4676a453ad14cbc7f3f78e417
brad <bradford.sawadye@jembi.org>
2020-03-06 12:47:58 +0200

    add command to create the namespace for the healthworkforce
    The command for deleting resources in a namespace as also been added

--- healthworkforce/kubernetes/main/k8s.sh
+++ healthworkforce/kubernetes/main/k8s.sh
@@ -3,11 +3,13 @@
 kustomizationFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "up" ]; then
+    # Create the namespace
+    kubectl apply -f $kustomizationFilePath/healthworkforce-namespace.yaml
     kubectl apply -k $kustomizationFilePath
 elif [ "$1" == "down" ]; then
     kubectl delete deployment mapper-deployment
 elif [ "$1" == "destroy" ]; then
-    kubectl delete -k $kustomizationFilePath
+    kubectl delete namespaces healthworkforce-component
 else
     echo "Valid options are: up, down, or destroy"
 fi
