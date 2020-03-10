041ffb37aaed6020a455e7bb471a02845ae40023
brad <bradford.sawadye@jembi.org>
2020-03-10 14:03:10 +0200

    implement namespace for dev scripts

--- core/kubernetes/dev/k8s.dev.sh
+++ core/kubernetes/dev/k8s.dev.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
 getExposedServices () {
-	mongoUrl="$(minikube service openhim-mongo-dev-service --url)"
+	mongoUrl="$(minikube service openhim-mongo-dev-service -n=core-package --url)"
 	trimmedMongoUrl=$(sed 's/http:\/\///g' <<< "$mongoUrl")
 	echo -e "The OpenHIM MongoDB url:\n $trimmedMongoUrl\n"
 
-	mysqlUrl="$(minikube service hapi-fhir-mysql-dev-service --url)"
+	mysqlUrl="$(minikube service hapi-fhir-mysql-dev-service -n=core-package --url)"
 	trimmedMysqlUrl=$(sed 's/http:\/\///g' <<< "$mysqlUrl")
 	echo -e "The HAPI FHIR MySQL url:\n $trimmedMysqlUrl\n"
 
-	echo -e "HAPI FHIR server is accessible at:\n https://$(kubectl get ingress hapi-fhir-server-ingress -o jsonpath="{..host}")/hapi-fhir-jpaserver/fhir/"
+	echo -e "HAPI FHIR server is accessible at:\n $(minikube service hapi-fhir-server-service -n=core-package --url)/hapi-fhir-jpaserver/fhir/"
 }
 
 applyDevScripts () {
-	kustomizationFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+	k8sDevRootFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 	if [ "$1" == "up" ]; then
-		kubectl apply -k $kustomizationFilePath
+		kubectl apply -k $k8sDevRootFilePath
 		echo -e "\nCurrently in development mode\n"
-
-		# create HOST entry for ingress
-    sudo sed -i "/HOST alias for kubernetes Minikube development/d" /etc/hosts
-    echo -e "\n$(minikube ip) $(kubectl get ingress hapi-fhir-server-ingress -o jsonpath="{..host}") # HOST alias for kubernetes Minikube development" | sudo tee -a /etc/hosts
 
 		getExposedServices
 	elif [ "$1" == "destroy" ]; then
 		# delete host entry on destroy
 		sudo sed -i "/HOST alias for kubernetes Minikube development/d" /etc/hosts
 
-		kubectl delete -k $kustomizationFilePath
+		kubectl delete -k $k8sDevRootFilePath
 	else
 		echo "Valid options are: up or destroy"
 	fi
