227a446efc4ceba36767e261aee2b98368ce9934
brad <bradford.sawadye@jembi.org>
2020-05-13 14:19:59 +0200

    add step for setting up the replica set to the core compose file
    This step has also been removed from the compose file which sets up the healthworkforce as both components use the same replica set
    
    OHIE-367

--- healthworkforce/docker/compose.sh
+++ healthworkforce/docker/compose.sh
@@ -1,11 +1,6 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "up" ]; then
-    docker-compose -f "$composeFilePath"/docker-compose-mongo.yml up -d
-
-    # Set up the replica set
-    "$composeFilePath"/initiateReplicaSet.sh
-
     docker-compose -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml  up -d
 elif [ "$1" == "down" ]; then
     docker-compose -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml stop
