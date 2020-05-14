2bfc54a07eb72955b1ce2d64c0da8270c1518ee8
bradsawadye <bradford.sawadye@jembi.org>
2020-05-14 11:51:10 +0200

    add underscore to unused parameter
    OHIE-367
    
    Co-authored-by: Matt <matthew.dickie@jembi.org>

--- healthworkforce/mock-services/mcsdMockServices/src/index.js
+++ healthworkforce/mock-services/mcsdMockServices/src/index.js
@@ -29,27 +29,27 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/gofr-location-mock/_history', function(req, res) {
+app.get('/gofr-location-mock/_history', function(_req, res) {
   logger.info(`Fetching GOFR Location data with query paramter`)
   res.json(locationResource)
 })
 
-app.get('/gofr-organization-mock/_history', function(req, res) {
+app.get('/gofr-organization-mock/_history', function(_req, res) {
   logger.info(`Fetching GOFR Organization data with query paramter`)
   res.json(organizationResource)
 })
 
-app.get('/ihris-practitioner-mock/_history', function(req, res) {
+app.get('/ihris-practitioner-mock/_history', function(_req, res) {
   logger.info(`Fetching iHRIS practitioner data with query paramter`)
   res.json(practitionerResource)
 })
 
-app.get('/ihris-practitionerRole-mock/_history', function(req, res) {
+app.get('/ihris-practitionerRole-mock/_history', function(_req, res) {
   logger.info(`Fetching iHRIS practitionerRole data with query paramter`)
   res.json(practitionerRoleResource)
 })
 
-app.post('/fhir-mock', function(req, res) {
+app.post('/fhir-mock', function(_req, res) {
   logger.info(`Processing FHIR transaction bundle...`)
   res.json(fhirTransactionBundleResultResource)
 })
