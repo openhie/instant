d565e66b10104ac01fd6c270d41d0407b823c4d7
bradsawadye <bradford.sawadye@jembi.org>
2020-05-18 15:23:19 +0200

    Update core/docker/compose.sh
    Co-authored-by: Matt <matthew.dickie@jembi.org>

--- core/docker/compose.sh
+++ core/docker/compose.sh
@@ -19,5 +19,5 @@ elif [ "$1" == "down" ]; then
 elif [ "$1" == "destroy" ]; then
     docker-compose -f "$composeFilePath"/docker-compose-mongo.yml -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml down -v
 else
-    echo "Valid options are: up, down, or destroy"
+    echo "Valid options are: init, up, down, or destroy"
 fi
