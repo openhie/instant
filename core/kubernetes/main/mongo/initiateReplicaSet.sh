2c701983032079476439d4e7231b83d137989476
brad <bradford.sawadye@jembi.org>
2020-05-21 14:10:38 +0200

    add scripts for setting up a mongo replica set
    A mongo replica set is now used for the db storage. The openhim core and the mapping mediator will use this replica set
    
    OHIE-395

---
+++ core/kubernetes/main/mongo/initiateReplicaSet.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+echo 'Initiating the mongo replica set'
+
+config='{"_id":"mongo-set","members":[{"_id":0,"priority":1,"host":"mongo-0.mongo:27017"},
+{"_id":1,"priority":0.5,"host":"mongo-1.mongo:27017"},{"_id":2,"priority":0.5,"host":"mongo-2.mongo:27017"}]}'
+
+# Sleep to ensure all the mongo instances for the replica set are up and running
+sleep 20
+
+kubectl exec -i mongo-0 -n core-package -- mongo --eval "rs.initiate($config)"
+
+## Allow the replica members to stable
+sleep 30
+
+echo 'Mongo Replica setup finished'
