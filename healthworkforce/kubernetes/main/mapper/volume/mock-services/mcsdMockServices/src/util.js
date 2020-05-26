ea3e1822aeb9f98134ef2e9f15b542bee5382582
brad <bradford.sawadye@jembi.org>
2020-05-26 18:35:13 +0200

    add the files for creating the mock services in the container The files will be mounted into the container and be used in running the mock services
    OHIE-395

---
+++ healthworkforce/kubernetes/main/mapper/volume/mock-services/mcsdMockServices/src/util.js
@@ -0,0 +1,14 @@
+'use strict'
+
+exports.parseStringToBoolean = (value, defaultValue) => {
+  if (!value) return defaultValue
+
+  switch (value.toLowerCase()) {
+    case 'true':
+      return true
+    case 'false':
+      return false
+    default:
+      return defaultValue
+  }
+}
