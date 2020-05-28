02197212b1d47ce02527640e49a1c8566bd01842
Martin Brocker <martin.brocker@jembi.org>
2020-05-28 16:23:36 +0200

    Updated mCSD endpoint mapping
    To cater for properly mapping the mCSD resources into a FHIR transaction bundle for processing in HAPI FHIR
    
    OHIE-395

--- core/kubernetes/main/mongo/initiateReplicaSet.sh
+++ core/kubernetes/main/mongo/initiateReplicaSet.sh
@@ -8,11 +8,11 @@ config='{"_id":"mongo-set","members":[{"_id":0,"priority":1,"host":"mongo-0.mong
 {"_id":1,"priority":0.5,"host":"mongo-1.mongo:27017"},{"_id":2,"priority":0.5,"host":"mongo-2.mongo:27017"}]}'
 
 # Sleep to ensure all the mongo instances for the replica set are up and running
-sleep 20
+sleep 60
 
 kubectl exec -i mongo-0 -- mongo --eval "rs.initiate($config)"
 
 ## Allow the replica members to stable
-sleep 30
+sleep 60
 
 echo 'Mongo Replica setup finished'
