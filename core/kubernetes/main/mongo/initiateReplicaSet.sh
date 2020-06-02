24a22f01dbcfe84534845186129a104d1cc6f1c8
brad <bradford.sawadye@jembi.org>
2020-06-02 11:39:22 +0200

    correct the configuration for initiating a replica set
    The hostnames of the mongo instances have been changed and this update had to be made
    
    OHIE-395

--- core/kubernetes/main/mongo/initiateReplicaSet.sh
+++ core/kubernetes/main/mongo/initiateReplicaSet.sh
@@ -4,8 +4,8 @@ set -eu
 
 echo 'Initiating the mongo replica set'
 
-config='{"_id":"mongo-set","members":[{"_id":0,"priority":1,"host":"mongo-0.mongo:27017"},
-{"_id":1,"priority":0.5,"host":"mongo-1.mongo:27017"},{"_id":2,"priority":0.5,"host":"mongo-2.mongo:27017"}]}'
+config='{"_id":"mongo-set","members":[{"_id":0,"priority":1,"host":"mongo-0.mongo-service:27017"},
+{"_id":1,"priority":0.5,"host":"mongo-1.mongo-service:27017"},{"_id":2,"priority":0.5,"host":"mongo-2.mongo-service:27017"}]}'
 
 # Sleep to ensure all the mongo instances for the replica set are up and running
 sleep 60
