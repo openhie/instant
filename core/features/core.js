498296507592d40c541ff32bcc5afe9dec5d3e65
MattyJ007 <matthew.dickie@jembi.org>
2020-07-16 13:42:49 +0200

    Add check for existing test patient
    If the test patient already exists in the DB do not create a new one.
    
    This will help with the clean up
    
    OHIE-422

--- core/features/core.js
+++ core/features/core.js
@@ -14,30 +14,55 @@ const OPENHIM_MEDIATOR_API_PORT =
 const CUSTOM_TOKEN_ID = process.env.CUSTOM_TOKEN_ID || 'test'
 
 Given('a patient, Jane Doe, exists in the FHIR server', async function () {
-  const options = {
-    url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_API_PORT}/hapi-fhir-jpaserver/fhir/Patient`,
-    method: 'POST',
+  const checkPatientExistsOptions = {
+    url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_TRANSACTION_API_PORT}/hapi-fhir-jpaserver/fhir/Patient?identifier:value=test`,
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Custom test`
-    },
-    data: {
-      resourceType: 'Patient',
-      name: {
-        use: 'temp',
-        family: 'Doe',
-        given: ['Jane']
-      },
-      identifier: {
-        use: 'temp',
-        value: 'test'
-      },
-      gender: 'male'
+      Authorization: `Custom ${CUSTOM_TOKEN_ID}`
     }
   }
 
-  const response = await axios(options)
-  expect(response.status).to.eql(201)
+  const checkPatientExistsResponse = await axios(checkPatientExistsOptions)
+
+  if (checkPatientExistsResponse.data.total === 0) {
+    console.log(
+      `Patient record for Jane Doe does not exist. Creating Patient...`
+    )
+    const options = {
+      url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_TRANSACTION_API_PORT}/hapi-fhir-jpaserver/fhir/Patient`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Custom ${CUSTOM_TOKEN_ID}`
+      },
+      data: {
+        resourceType: 'Patient',
+        name: {
+          use: 'temp',
+          family: 'Doe',
+          given: ['Jane']
+        },
+        identifier: {
+          use: 'temp',
+          value: 'test'
+        },
+        gender: 'male'
+      }
+    }
+
+    const response = await axios(options)
+    expect(response.status).to.eql(201)
+  } else if (checkPatientExistsResponse.data.total === 1) {
+    console.log(
+      `Patient record for Jane Doe already exists...`
+    )
+  } else {
+    // Previous test data should have been cleaned out
+    throw new Error(
+      `Multiple Patient records for Jane Doe exist: ${checkPatientExistsResponse.data.total}`
+    )
+  }
 })
 
 Given('an authorised client, Alice, exists in the OpenHIM', function () {
