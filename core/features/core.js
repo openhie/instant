f15d4507f29b3130a5993d0fca29666b984324e9
MattyJ007 <matthew.dickie@jembi.org>
2020-07-16 11:03:02 +0200

    Add initial Given assertion
    Check that the we can access HAPI via the OpenHIM for testing
    
    OHIE-422

--- core/features/core.js
+++ core/features/core.js
@@ -1,8 +1,40 @@
+'use strict'
+
+const axios = require('axios')
+
 const { Given, When, Then } = require('cucumber')
 const { expect } = require('chai')
 
-Given('a patient, Jane Doe, exists in the FHIR server', function () {
-  return 'pending'
+const OPENHIM_PROTOCOL = process.OPENHIM_PROTOCOL || 'http'
+const OPENHIM_API_HOSTNAME = process.OPENHIM_API_HOSTNAME || 'localhost'
+const OPENHIM_API_PORT = process.OPENHIM_API_PORT || '5001'
+
+
+Given('a patient, Jane Doe, exists in the FHIR server', async function () {
+  const options = {
+    url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_API_PORT}/hapi-fhir-jpaserver/fhir/Patient`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Custom test`
+    },
+    data: {
+      resourceType: 'Patient',
+      name: {
+        use: 'temp',
+        family: 'Doe',
+        given: ['Jane']
+      },
+      identifier: {
+        use: 'temp',
+        value: 'test'
+      },
+      gender: 'male'
+    }
+  }
+
+  const response = await axios(options)
+  expect(response.status).to.eql(201)
 })
 
 Given('an authorised client, Alice, exists in the OpenHIM', function () {
