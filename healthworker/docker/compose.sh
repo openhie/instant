c5d1d4775e83bb9dace5208de0531d8a1a70c7d0
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2020-11-03 15:10:17 -0500

    edit compose.sh

--- healthworker/docker/compose.sh
+++ healthworker/docker/compose.sh
@@ -3,23 +3,21 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    # docker-compose -p healthworker -f "$composeFilePath"/docker-compose-mongo.yml up -d
 
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.hapi.yml up -d
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.hapi.config.yml up
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.config.yml up
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.data.yml up
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml up -d
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.yml up
-    # need way to clear existed config stuff
+    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.yml up -d
 
 
 elif [ "$1" == "up" ]; then
 
-    # Wait for mongo replica set to be set up
-    # sleep 20
-
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.hapi.yml up -d
+    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.hapi.config.yml up
+    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.config.yml up
+    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.data.yml up
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml up -d
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.ihris.yml up -d
 
@@ -31,8 +29,6 @@ elif [ "$1" == "down" ]; then
 
 
 elif [ "$1" == "destroy" ]; then
-
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose-mongo.yml -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml down -v
 
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.hapi.yml down -v
     docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml down -v
