34d271cb038f1777310feaef7892ab2fd9995c7e
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2020-11-26 17:34:53 -0500

    Fix to compose.sh for client registry to be launched to be orchestration process

--- client/docker/compose.sh
+++ client/docker/compose.sh
@@ -4,21 +4,21 @@ composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
 
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p client -f "$composeFilePath"/docker-compose.yml up -d
 
 
 elif [ "$1" == "up" ]; then
 
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p client -f "$composeFilePath"/docker-compose.yml up -d
 
 elif [ "$1" == "down" ]; then
 
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.yml stop
+    docker-compose -p client -f "$composeFilePath"/docker-compose.yml stop
 
 
 elif [ "$1" == "destroy" ]; then
 
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.yml down -v
+    docker-compose -p client -f "$composeFilePath"/docker-compose.yml down -v
 
 else
     echo "Valid options are: init, up, down, or destroy"
