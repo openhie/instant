972e1409e2710415e095bb885d5a595b0fe55d66
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2020-12-09 14:08:31 -0500

    clean up JS, fix headers

--- goinstant/templates/main.js
+++ goinstant/templates/main.js
@@ -1,3 +1,11 @@
+function Setup() {
+    var url = "http://localhost:27517/index.html";
+    window.location = url;
+    fetch("http://localhost:27517/disclaimer")
+    fetch("http://localhost:27517/setup")
+}
+
+
 function accept() {
     // document.getElementById("accept").innerHTML = "Hello World";
     fetch('http://localhost:27517/api/makedisclaimer')
@@ -20,29 +28,51 @@ function accept() {
         });
 }
 
+var source = new EventSource('http://localhost:27517/events?stream=messages');
+source.onmessage = function (e) {
+    document.getElementById('console').innerHTML += e.data + '<br>';
+};
 
-function packageTotal() {
-
-    if (document.getElementById('basicProgram').checked) {
-        // Basic package is checked
-        window.location.href = "http://localhost:27517/usecase.html";
-
-    } else if (document.getElementById('proProgram').checked) {
-        // Pro package is checked
-        window.location.href = "http://localhost:27517/debug.html";
-    }
+function Setup() {
+    fetch("http://localhost:27517/setup")
 }
 
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+function DebugDocker() {
+    fetch("http://localhost:27517/debugdocker")
 }
+
+function DebugKubernetes() {
+    fetch("http://localhost:27517/debugkubernetes")
+}
+
+function ComposeUpCoreDOD() {
+    fetch("http://localhost:27517/composeupcoredod")
+}
+
+
+
+// function packageTotal() {
+
+//     if (document.getElementById('basicProgram').checked) {
+//         // Basic package is checked
+//         window.location.href = "http://localhost:27517/usecase.html";
+
+//     } else if (document.getElementById('proProgram').checked) {
+//         // Pro package is checked
+//         window.location.href = "http://localhost:27517/debug.html";
+//     }
+// }
+
+// function openCity(evt, cityName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
