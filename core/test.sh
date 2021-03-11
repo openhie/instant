d925afe17fe30a39f6ac0b0dcd8765c9463fb666
MattyJ007 <matthew.dickie@jembi.org>
2021-03-11 15:00:58 +0200

    Change core test package variable name and content
    The FHIR server will not always be on an SSL port. When testing it will
    be http. Therefore we need to be able to pass in the protocol as well as
    the hostname in to the test
    
    DEB-20

--- core/test.sh
+++ core/test.sh
@@ -1,5 +1,5 @@
 echo -e "\nHAPI FHIR test..."
 
-HOST=$1
+URLBASE=$1
 
-curl https://$HOST/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ "resourceType": "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
+curl $URLBASE/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ "resourceType": "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
