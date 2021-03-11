4f32a3be0e0519ca8aa5712d84da7fc234922b46
MattyJ007 <matthew.dickie@jembi.org>
2021-03-11 14:59:03 +0200

    Fix broken core test data
    A JSON payload requires the field name to be surrounded by double quotes
    
    DEB-20

--- core/test.sh
+++ core/test.sh
@@ -2,4 +2,4 @@ echo -e "\nHAPI FHIR test..."
 
 HOST=$1
 
-curl https://$HOST/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ resourceType: "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
+curl https://$HOST/fhir/Patient -k -H "Content-Type: application/fhir+json" -f -d '{ "resourceType": "Patient" }' && echo -e '\n\nSUCCESS' || (echo -e '\nFAILED'; exit 1)
