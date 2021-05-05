a42213db76e4f9318171b817530a6d3551d1e626
Ryan Crichton <ryan@jembi.org>
2021-05-05 13:19:54 +0200

    Update OpenCR version and fix/complete k8s scripts
    The kubernetes deploy for OpenCR was incomplete, this change completes it and restructures it to use the latest version of OpenCR and to play nicely with cloud environments.

--- client/kubernetes/main/k8s.sh
+++ client/kubernetes/main/k8s.sh
@@ -5,6 +5,7 @@ k8sMainRootFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 if [ "$1" == "init" ]; then
 
     kubectl apply -k $k8sMainRootFilePath
+    bash "$k8sMainRootFilePath"/../importer/k8s.sh up
 
 elif [ "$1" == "up" ]; then
 
@@ -17,7 +18,8 @@ elif [ "$1" == "down" ]; then
 elif [ "$1" == "destroy" ]; then
 
     kubectl delete -k $k8sMainRootFilePath
-    
+    bash "$k8sMainRootFilePath"/../importer/k8s.sh clean
+
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
