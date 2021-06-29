70a5d7f9b8248d682894645cfaf379a7ea82e0a8
Ryan Crichton <ryan@jembi.org>
2021-06-29 10:15:04 +0200

    Add base OpenHIM config importers to sync raw resoruces with es
    DIO-22

--- elastic-pipeline/docker/compose.sh
+++ elastic-pipeline/docker/compose.sh
@@ -11,13 +11,13 @@ if [ "$1" == "init" ]; then
     docker cp "$composeFilePath"/pipelines.yml logstash-helper:/config/
     docker rm logstash-helper
 
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
 elif [ "$1" == "up" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 elif [ "$1" == "down" ]; then
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml stop
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml stop
 elif [ "$1" == "destroy" ]; then
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml down -v
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml down -v
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
---
+++ elastic-pipeline/docker/importer/Dockerfile
@@ -0,0 +1,9 @@
+FROM node:erbium-alpine
+
+WORKDIR /importer
+
+RUN npm install -g wait-on@5.0.1
+
+RUN npm install -g axios@0.19.2
+
+ENV NODE_PATH ../../usr/local/lib/node_modules
