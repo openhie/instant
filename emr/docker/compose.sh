d160e2252a8fbba77bce252425b6f3d149452073
mozzy11 <mozzymutesa@gmail.com>
2021-10-07 18:00:47 +0300

    clean up

--- emr/docker/compose.sh
+++ emr/docker/compose.sh
@@ -3,17 +3,16 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker-compose -p emr -f "$composeFilePath"/docker-compose.yml up -d
-
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 
 elif [ "$1" == "up" ]; then
-    docker-compose -p emr -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 
 elif [ "$1" == "down" ]; then
-    docker-compose -p emr -f "$composeFilePath"/docker-compose.yml stop
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml stop
 
 elif [ "$1" == "destroy" ]; then
-    docker-compose -p emr -f "$composeFilePath"/docker-compose.yml down -v
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml down -v
 
 else
     echo "Valid options are: init, up, down, or destroy"
