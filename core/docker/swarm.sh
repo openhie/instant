b39d47da9a48d6c7e9eb927e5f942485398ac642
MattyJ007 <matthew.dickie@jembi.org>
2021-10-28 15:43:15 +0200

    Split out the swarm init command in stages
    Give the services time to start up before their dependants start up.
    
    Also remove the .dev compose script from the start up as those ports
    should not be exposed in prod

--- core/docker/swarm.sh
+++ core/docker/swarm.sh
@@ -8,7 +8,14 @@ if [ "$1" == "init" ]; then
     # Set up the replica set
     "$composeFilePath"/initiateReplicaSet.sh
 
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/importer/docker-compose.config.yml -c "$composeFilePath"/docker-compose.stack-1.yml -c "$composeFilePath"/importer/docker-compose.config.stack-0.yml instant
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
+
+    echo "Sleep 60 seconds to give OpenHIM Core and MySQL time to start up before OpenHIM Console and HAPI-FHIR run"
+    sleep 60
+
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+
+    docker stack deploy -c "$composeFilePath"/importer/docker-compose.config.yml -c "$composeFilePath"/importer/docker-compose.config.stack-0.yml instant
 
     echo "Sleep 60 seconds to give core config importer time to run before cleaning up service"
     sleep 60
@@ -19,7 +26,7 @@ elif [ "$1" == "up" ]; then
 
     # Wait for mongo replica set to be set up
     sleep 20
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
 elif [ "$1" == "down" ]; then
     docker service scale instant_openhim-core=0 instant_openhim-console=0 instant_hapi-fhir=0 instant_hapi-mysql=0 instant_mongo-1=0 instant_mongo-2=0 instant_mongo-3=0
 elif [ "$1" == "destroy" ]; then
