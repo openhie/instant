b883e0ad3b12ca8ffc8720728da1ea567282f81e
bradsawadye <bradsawadye@gmail.com>
2021-11-04 14:30:15 +0200

    Remove script for setting up replica set
    We now use one instance of mongo

--- core/docker/compose.sh
+++ core/docker/compose.sh
@@ -5,9 +5,6 @@ composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 if [ "$1" == "init" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose-mongo.yml up -d
 
-    # Set up the replica set
-    "$composeFilePath"/initiateReplicaSet.sh
-
     docker create --name hapi-mysql-helper -v hapi-mysql-config:/conf.d busybox
     docker cp "$composeFilePath"/importer/volume/mysql.cnf hapi-mysql-helper:/conf.d/mysql.cnf
     docker rm hapi-mysql-helper
@@ -15,9 +12,6 @@ if [ "$1" == "init" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
 elif [ "$1" == "up" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose-mongo.yml up -d
-
-    # Wait for mongo replica set to be set up
-    sleep 20
 
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml up -d
 elif [ "$1" == "down" ]; then
