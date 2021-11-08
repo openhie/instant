a350005427c1dcfacad1d18698f98e3a5141144d
mozzy11 <mozzymutesa@gmail.com>
2021-11-08 17:10:41 +0300

    Add Openhim config for the facility package

--- facility/docker/compose.sh
+++ facility/docker/compose.sh
@@ -3,13 +3,13 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker-compose -p facility -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
 
     # Set up the openhim
     # "$composeFilePath"/initiateReplicaSet.sh
 
 elif [ "$1" == "up" ]; then
-    docker-compose -p facility -f "$composeFilePath"/docker-compose.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 
     # Wait
     # sleep 20
@@ -17,10 +17,10 @@ elif [ "$1" == "up" ]; then
     # do something else needed
 
 elif [ "$1" == "down" ]; then
-    docker-compose -p facility -f "$composeFilePath"/docker-compose.yml stop
+     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml stop
 
 elif [ "$1" == "destroy" ]; then
-    docker-compose -p facility -f "$composeFilePath"/docker-compose.yml down -v
+     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml down -v
 
 else
     echo "Valid options are: init, up, down, or destroy"
