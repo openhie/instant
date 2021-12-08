5fbd442bcc583644cc09829b79670fa42876f1a6
mozzy11 <mozzymutesa@gmail.com>
2021-12-08 16:06:32 +0300

    update instant packages

--- emr/docker/compose.sh
+++ emr/docker/compose.sh
@@ -3,6 +3,14 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
+    docker create --name openmrs-db-helper -v openmrs-dbdump:/dbdump busybox
+    docker cp "$composeFilePath"/importer/volume/dbdump/. openmrs-db-helper:/dbdump/
+    docker rm openmrs-db-helper
+
+    docker create --name openmrs-server-helper -v openmrs-modules:/modules busybox
+    docker cp "$composeFilePath"/importer/volume/modules/. openmrs-server-helper:/modules/
+    docker rm openmrs-server-helper
+
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 
 elif [ "$1" == "up" ]; then
