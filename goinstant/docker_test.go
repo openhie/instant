5994487f1eddb4f685ddff0f31520abeb446f678
mark-labuschagne <marklabuschagne7@gmail.com>
2022-01-25 09:38:30 +0200

    Remove integration test from go cli. The removed test is written in Gherkin, on another branch. The test does not belong here. Plat-37

--- goinstant/docker_test.go
+++ goinstant/docker_test.go
@@ -7,116 +7,11 @@ import (
 	"io"
 	"io/fs"
 	"log"
-	"net/http"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-/*
-	Instead of running the test below using the buttons (if you have extensions installed), run the test using this command
-	cmd:	go test -timeout 2m -v -run ^TestRunDirectDockerCommand$ github.com/openhie/instant/goinstant
-	so that the timeout can be set manually
-*/
-
-type testingStruct struct {
-	cmds                     []string
-	testInfo                 string
-	heartbeatWantedBefore    bool
-	heartbeatNotWantedBefore bool
-	heartbeatWantedAfter     bool
-	heartbeatNotWantedAfter  bool
-}
-
-func TestRunDirectDockerCommand(t *testing.T) {
-	loadConfig()
-
-	type args struct {
-		startupCommands          []string
-		heartbeatWantedBefore    bool
-		heartbeatNotWantedBefore bool
-		heartbeatWantedAfter     bool
-		heartbeatNotWantedAfter  bool
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "Test 1: Attempt to init OpenHIM Core",
-			args: args{
-				startupCommands:          []string{"docker", "core", "init"},
-				heartbeatNotWantedBefore: true,
-				heartbeatWantedAfter:     true,
-			},
-			wantErr: false,
-		},
-		{
-			name: "Test 2: Attempt to bring OpenHIM Core down",
-			args: args{
-				startupCommands:         []string{"docker", "core", "down"},
-				heartbeatWantedBefore:   true,
-				heartbeatNotWantedAfter: true,
-			},
-			wantErr: false,
-		},
-		{
-			name: "Test 3: Attempt to bring OpenHIM Core up.",
-			args: args{
-				startupCommands:          []string{"docker", "core", "up"},
-				heartbeatNotWantedBefore: true,
-				heartbeatWantedAfter:     true,
-			},
-			wantErr: false,
-		},
-		{
-			name: "Test 4: Attempt to destroy OpenHIM Core.",
-			args: args{
-				startupCommands:         []string{"docker", "core", "destroy"},
-				heartbeatWantedBefore:   true,
-				heartbeatNotWantedAfter: true,
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		os.Stdout = nil
-
-		t.Run(tt.name, func(t *testing.T) {
-			hbCheck := CheckOpenHIMheartbeat()
-			if tt.args.heartbeatWantedBefore {
-				if !hbCheck {
-					t.Fatal("Expected heartbeat and not found")
-				}
-			}
-			if tt.args.heartbeatNotWantedBefore {
-				if hbCheck {
-					t.Fatal("Heartbeat found when not expected")
-				}
-			}
-
-			if err := RunDirectDockerCommand(tt.args.startupCommands); (err != nil) != tt.wantErr {
-				t.Errorf("RunDirectDockerCommand() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			hbCheck = CheckOpenHIMheartbeat()
-			if tt.args.heartbeatWantedAfter {
-				if !hbCheck {
-					t.Fatal("Expected heartbeat and not found")
-				}
-			}
-			if tt.args.heartbeatNotWantedAfter {
-				if hbCheck {
-					t.Fatal("Heartbeat found when not expected")
-				}
-			}
-
-			t.Log(t.Name() + " passed!\n")
-		})
-	}
-}
 
 func Test_sliceContains(t *testing.T) {
 	testCases := []struct {
@@ -149,19 +44,6 @@ func Test_sliceContains(t *testing.T) {
 			t.Log(tt.testInfo + " passed!")
 		})
 	}
-}
-
-func CheckOpenHIMheartbeat() bool {
-	resp, err := http.Get("http://localhost:9000")
-	if resp == nil || resp.StatusCode != 200 {
-		return false
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	return true
 }
 
 func Test_getPackagePaths(t *testing.T) {
