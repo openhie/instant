474cd2a42df1944a47150d3297f962f4faf2a35e
MattyJ007 <matthew.dickie@jembi.org>
2022-02-01 14:05:22 +0200

    Add generic custom package test handling in go tests
    Ensure that custom package test scenarios can easily be tested in
    non-interactive cli mode
    
    PLAT-37

--- goinstant/features/service_options.feature
+++ goinstant/features/service_options.feature
@@ -3,7 +3,17 @@ Feature: Test Deploy Options Commands
     When the command "init core -t=docker --dev" is run
     Then check the CLI output is "init --dev -t docker core"
 
-  Scenario: Initialise Custom Service in Dev Mode
+  Scenario: Initialise Template Custom Service
     When the command "init template -t=docker -c=https://github.com/jembi/instant-openhie-template-package.git" is run
-    Then check the CLI output is "3rd party packages to instant volume:- https://github.com/jembi/instant-openhie-template-package.git"
+    Then check that the CLI added custom packages
+      | directory | location |
+      | instant-openhie-template-package | https://github.com/jembi/instant-openhie-template-package.git |
     Then check the CLI output is "init -t docker template"
+
+  Scenario: Initialise Multiple Services
+    When the command "init covid19immunization client covid19surveillance -t=docker --custom-package=https://github.com/jembi/covid19-immunization-tracking-package.git -c=https://github.com/jembi/who-covid19-surveillance-package.git" is run
+    Then check that the CLI added custom packages
+      | directory | location |
+      | covid19-immunization-tracking-package | https://github.com/jembi/covid19-immunization-tracking-package.git |
+      | who-covid19-surveillance-package | https://github.com/jembi/who-covid19-surveillance-package.git |
+    Then check the CLI output is "init -t docker covid19immunization client covid19surveillance"
--- goinstant/godogs_test.go
+++ goinstant/godogs_test.go
@@ -18,6 +18,7 @@ import (
 var (
 	binaryFilePath string
 	logs           string
+	directoryNames []string
 )
 
 func theCommandIsRun(command string) error {
@@ -33,6 +34,31 @@ func checkTheCLIOutputIs(expectedOutput string) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func checkCustomPackages(packages *godog.Table) error {
+	head := packages.Rows[0].Cells
+
+	for i := 1; i < len(packages.Rows); i++ {
+
+		for n, cell := range packages.Rows[i].Cells {
+			switch head[n].Value {
+			case "directory":
+				directoryNames = append(directoryNames, cell.Value)
+			case "location":
+				err := compareStrings(logs, cell.Value)
+				if err != nil {
+					return err
+				}
+			default:
+				return errors.New("Unexpected column name: " + head[n].Value)
+			}
+		}
+	}
+
+	// Cleanup downloaded packages post test
+	deleteContentAtFilePath([]string{".", "features"}, directoryNames)
 	return nil
 }
 
@@ -54,6 +80,7 @@ func InitializeScenario(sc *godog.ScenarioContext) {
 			}
 			sc.Step(`^check the CLI output is "([^"]*)"$`, checkTheCLIOutputIs)
 			sc.Step(`^the command "([^"]*)" is run$`, theCommandIsRun)
+			sc.Step(`^check that the CLI added custom packages$`, checkCustomPackages)
 		},
 	}
 
@@ -134,9 +161,15 @@ func deleteContentAtFilePath(filePath []string, content []string) {
 
 func cleanUp() {
 	deleteContentAtFilePath([]string{".", "features"}, []string{"test-platform.exe", "test-platform-linux", "test-platform-macos"})
-	deleteContentAtFilePath([]string{"."}, []string{"instant-openhie-template-package"})
-	_, err := runTestCommand("docker", "volume", "rm", "instant")
-	if err != nil {
-		fmt.Println("Volume not deleted")
+	deleteContentAtFilePath([]string{"."}, directoryNames)
+
+	_, errContainer := runTestCommand("docker", "rm", "instant-openhie")
+	if errContainer == nil {
+		fmt.Println("Deleted Instant OpenHIE container")
+	}
+
+	_, errVolume := runTestCommand("docker", "volume", "rm", "instant")
+	if errVolume != nil {
+		fmt.Println("Instant Docker volume not deleted")
 	}
 }
