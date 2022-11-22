3ecfb06041a985e4ae15aa0d9f815079e1cc5488
Norman Hooper <nhooper@dimagi.com>
2022-11-22 23:59:20 +0000

    Always get the latest. Just get the one you want.

--- get-cli.sh
+++ get-cli.sh
@@ -1,8 +1,17 @@
-cliVersion=0.1.0
+#!/bin/bash
 
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-linux -o ./goinstant-linux
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-macos -o ./goinstant-macos
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli.exe -o ./goinstant.exe
-chmod +x ./goinstant-linux
-chmod +x ./goinstant-macos
-chmod +x ./goinstant.exe
+if [ "$1" == "linux" ]; then
+    curl -L https://github.com/openhie/package-starter-kit/releases/download/latest/gocli-linux -o goinstant
+    chmod +x goinstant
+
+elif [ "$1" == "macos" ]; then
+    curl -L https://github.com/openhie/package-starter-kit/releases/download/latest/gocli-macos -o goinstant
+    chmod +x goinstant
+
+elif [ "$1" == "windows" ]; then
+    curl -L https://github.com/openhie/package-starter-kit/releases/download/latest/gocli.exe -o goinstant.exe
+    chmod +x ./goinstant.exe
+
+else
+    echo 'Usage: ./get-cli.sh "linux|macos|windows"'
+fi
